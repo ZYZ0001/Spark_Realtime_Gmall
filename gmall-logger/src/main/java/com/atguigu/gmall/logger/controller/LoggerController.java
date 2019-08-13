60d9ed2c25b5c0d1c6faae4a9f220895906c849e
ZYZ0001 <599212845@qq.com>
2019-08-13 08:32:32 +0800

    日志生成V1.0

--- gmall-logger/src/main/java/com/atguigu/gmall/logger/controller/LoggerController.java
+++ gmall-logger/src/main/java/com/atguigu/gmall/logger/controller/LoggerController.java
@@ -2,8 +2,10 @@ package com.atguigu.gmall.logger.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.atguigu.gmall.common.constant.GmallConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,20 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoggerController {
 
     @Autowired
-//KafkaTemplate<String, String> kafkaTemplate;
+    KafkaTemplate<String, String> kafkaTemplate;
 
     @PostMapping("log")
     public String dolog(@RequestParam("logString") String logString) {
 
-        // 补充时间戳
+        // TODO 1.补充时间戳
         JSONObject jsonObject = JSON.parseObject(logString);
         jsonObject.put("ts", System.currentTimeMillis());
 
-        // 落盘
-        log.info(jsonObject.toJSONString());
+        // TODO 2.落盘
+        String jsonString = jsonObject.toJSONString();
+        log.info(jsonString);
 
-        // 推送到kafka
-
+        // TODO 3.推送到kafka
+        if ("startup".equals(jsonObject.getString("type"))) {
+            // 启动日志
+            kafkaTemplate.send(GmallConstants.KAFKA_TOPIC_STARTUP, jsonString);
+        } else if ("event".equals(jsonObject.getString("type"))) {
+            // 事件日志
+            kafkaTemplate.send(GmallConstants.KAFKA_TOPIC_EVENT, jsonString);
+        }
 
         return "success";
     }
